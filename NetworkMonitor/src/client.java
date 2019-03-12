434d01d9cc4e1e792f367945ee66b1c87b7f7a8f
Barlow <1016209331@qq.com>
2019-03-13 00:30:15 +0800

    complete project NetworkMontior

--- NetworkMonitor/src/client.java
+++ NetworkMonitor/src/client.java
@@ -7,17 +7,21 @@ import java.awt.event.WindowEvent;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.Date;
 
 public class client {
     static public void main(String[] a) {
+        //启动四个窗口，代表四个客户端
         clientFrame cfa = new clientFrame("A");
+        cfa.setLocation(300,500);
         cfa.setVisible(true);
         clientFrame cfb = new clientFrame("B");
+        cfb.setLocation(600,500);
         cfb.setVisible(true);
         clientFrame cfc = new clientFrame("C");
+        cfc.setLocation(900,500);
         cfc.setVisible(true);
         clientFrame cfd = new clientFrame("D");
+        cfd.setLocation(1200,500);
         cfd.setVisible(true);
     }
 }
@@ -26,15 +30,15 @@ class clientFrame extends JFrame {
     final private int WIDTH = 250;
     final private int HEIGHT = 180;
     private Boolean connet = false;
-    private Panel p;
-    private JButton jb = new JButton("上线");
+    private Panel p; //指示灯
+    private JButton jb = new JButton("上线"); //上线按钮
     private Font f = new Font("宋体", Font.BOLD, 30);
-    static Socket cs = null;
+    private Socket cs = null; //与服务器通讯的socket
 
     public clientFrame(String title) {
         setTitle(title);
         setResizable(false);
-        p = new Panel(title);
+        p = new Panel(title); //设置标识
         //设置布局:不适用默认布局，完全自定义
         setLayout(null);
         setSize(WIDTH, HEIGHT);
@@ -44,47 +48,78 @@ class clientFrame extends JFrame {
         jb.setBounds(110, 30, 90, 90);
         jb.setBorder(BorderFactory.createEtchedBorder());
         this.add(jb);
-        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        addWindowListener(new WindowAdapter() {   //关闭窗口时，关闭套接字
+                              @Override
+                              public void windowClosing(WindowEvent e) {
+                                  try{
+                                      if (cs != null)  //关闭连接
+                                          cs.close();
+                                  }catch (Exception cse){
+                                      cse.printStackTrace();
+                                  }
+                                  dispose();
+                              }
+                          });
+        //添加按钮监听事件
         jb.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if (!connet) {
+                if (!connet) { //如果未连接
                     try {
-                        cs = new Socket(InetAddress.getLocalHost(), 10000);
-                        cs.setKeepAlive(true);
+                        cs = new Socket(InetAddress.getLocalHost(), 10000);  //请求与服务器连接
+                        cs.setKeepAlive(true); //保持连接
                         OutputStream os = cs.getOutputStream();//字节输出流
-                        os.write(title.getBytes());
+                        os.write(title.getBytes());  //向服务器表明身份
                         os.flush();
-                        new Thread(new clientThread(cs)).start();
-                    } catch (Exception ex) {
+                        new Thread(new clientThread(cs)).start(); //启动心跳线程
+                    } catch (Exception ex) {  //连接失败
                         connet = false;
                         jb.setText("上线");
                         p.setColor(Color.red);
-                        ex.printStackTrace();
+                      //  ex.printStackTrace();
                     }
                     connet = true;
                     jb.setText("下线");
-                    p.setColor(Color.green);
-                } else {
+                    p.setColor(Color.green);  //改变指示灯颜色
+                } else {  //如果已连接
                     try {
-                        if (cs != null)
+                        if (cs != null)  //关闭连接
                             cs.close();
                     } catch (Exception ex) {
                         ex.printStackTrace();
                     }
                     connet = false;
                     jb.setText("上线");
-                    p.setColor(Color.red);
+                    p.setColor(Color.red);//改变指示灯颜色
                 }
             }
         });
     }
-
-    public void setClientState(Boolean b) {
+    class clientThread implements Runnable {  //心跳线程，定期向服务器发送心跳，确认自己在线
+        Socket cs;
+        public  clientThread(Socket s){
+            this.cs=s;
+        }
+        @Override
+        public void run() {
+            OutputStream out=null;
+            try {
+                while (true) {  //间隔1秒发送一次心跳
+                    if(cs!=null){
+                        out=cs.getOutputStream();
+                        out.write("A".getBytes());
+                        out.flush();
+                        cs.getInputStream().read(new byte[128]);
+                        Thread.sleep(1000);
+                    }else {
+                        break;
+                    }
+                }
+            } catch (Exception e) {  //如果连接被关闭，结束线程
+              //  e.printStackTrace();
+            }
+        }
+    }
+    public void setClientState(Boolean b) {  //该变指示灯颜色
         p.setColor(b ? Color.green : Color.red);
     }
 
@@ -116,40 +151,14 @@ class clientFrame extends JFrame {
         }
 
         public void setColor(Color c) {
-            color = c;
-            updateUI();
-
+            color = c;  //设置颜色
+            updateUI(); //刷新界面
         }
 
-        public void drawCircle(Graphics g) {
+        public void drawCircle(Graphics g) {  //画圆
             Graphics2D g2d = (Graphics2D) g.create();
             g2d.setColor(color);
             g2d.fillOval(10, 0, r, r);
-        }
-    }
-}
-class clientThread implements Runnable {
-    Socket cs;
-    public  clientThread(Socket s){
-        this.cs=s;
-    }
-    @Override
-    public void run() {
-        OutputStream out=null;
-        try {
-            while (true) {
-                if(cs!=null){
-                    out=cs.getOutputStream();
-                    out.write("A".getBytes());
-                    out.flush();
-                    cs.getInputStream().read(new byte[128]);
-                    Thread.sleep(2000);
-                }else {
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
--- NetworkMonitor/src/server.java
+++ NetworkMonitor/src/server.java
@@ -7,30 +7,25 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class server {
-    static volatile boolean[] clientStatus = {false,false,false,false};
-    static synchronized void changStatus(int i,boolean b){
-        clientStatus[i]=b;
-    }
     static public void main(String[] args) {
-        serverFrame s = new serverFrame();
+        serverFrame s = new serverFrame(); //建立服务器窗口
         s.setVisible(true);
         try {
-            ServerSocket socket = new ServerSocket(10000);
+            ServerSocket socket = new ServerSocket(10000);  //监听10000端口
             byte[] buf = new byte[128];
             while (true) {
                 //接收客户端Socket
                 Socket cs = socket.accept();
                 InputStream in = cs.getInputStream();
                 int len = in.read(buf);
-                String info = new String(buf, 0, len);
+                String info = new String(buf, 0, len);  //接受客户端标识
                 char[] chars = info.toCharArray();
-                new Thread(new serverThread(cs, s, chars[0])).start();
-
+                s.setClientState( chars[0], true);
+                new Thread(new serverThread(cs, s, chars[0])).start();  //为客户端建立线程
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
 
@@ -51,27 +46,19 @@ class serverThread implements Runnable {
             while (true) {
                     if (cs != null) {
                         InputStream in = cs.getInputStream();
-                        int len = 0;
                         try {
-                            len = in.read(buf);
-                            Thread.sleep(500);
-                            cs.getOutputStream().write("ACK".getBytes());
-                           // String info = new String(buf, 0, len);
-                        } catch (Exception ioe) {
-                            sf.setClientState((char) (id), false);
-                            server.changStatus(id - 'A', false);
-                            cs.close();
-                            ioe.printStackTrace();
-                            System.out.println((char)id);
+                            in.read(buf);  //读取客户端心跳，如果读取失败，说明客户端下线
+                            cs.getOutputStream().write("ACK".getBytes()); //回复心跳
+                        } catch (Exception ioe) {  //如果读取心跳失败
+                            cs.close(); //关闭套接字
+                         //   ioe.printStackTrace();
                             break;
                         }
-                        sf.setClientState((char) (id), true);
                     } else {
-                        if(server.clientStatus[id - 'A'])sf.setClientState((char) (id), false);
-                        System.out.println((char)id);
                         break;
                     }
                 }
+            sf.setClientState((char) (id), false); //如果跳出循环，说明断开连接，将灯变为红色
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -83,7 +70,6 @@ class serverFrame extends JFrame {
     final int WIDTH = 300;
     final int HEIGHT = 180;
     private Panel p = new Panel();
-    public Socket[] csarr = new Socket[4];
 
     public serverFrame() {
         setTitle("服务器");
@@ -106,15 +92,15 @@ class serverFrame extends JFrame {
         p.setColor(a, b ? Color.green : Color.red);
     }
 
-    class Panel extends JPanel {
+    class Panel extends JPanel {  //指示灯面板
         final int PanelWIDTH = 240;
         final int PanelHEIGHT = 120;
         final int r = 40;
-        private JLabel pa;
+        private JLabel pa;  //四个标识符区域
         private JLabel pb;
         private JLabel pc;
         private JLabel pd;
-        private Color[] color = new Color[4];
+        private Color[] color = new Color[4];  //四个指示灯颜色
         private Font f = new Font("Times New Roman", Font.BOLD, 16);
 
         public Panel() {
@@ -140,25 +126,24 @@ class serverFrame extends JFrame {
             this.add(pb);
             this.add(pc);
             this.add(pd);
-            color[0] = Color.red;
+            color[0] = Color.red;  //初始为红色
             color[1] = Color.red;
             color[2] = Color.red;
             color[3] = Color.red;
         }
 
         @Override
-        protected void paintComponent(Graphics g) {
+        protected void paintComponent(Graphics g) {  //初始化和调用updateUI()时，系统会调用此方法重绘面板
             super.paintComponent(g);
             drawCircle(g);
         }
 
-        public void setColor(char a, Color c) {
+        public void setColor(char a, Color c) {  //设置灯颜色
             color[a - 'A'] = c;
             updateUI();
-
         }
 
-        public void drawCircle(Graphics g) {
+        public void drawCircle(Graphics g) {  //画出四个指示灯
             Graphics2D g2d = (Graphics2D) g.create();
             g2d.setColor(color[0]);
             g2d.fillOval(10, 0, r, r);
